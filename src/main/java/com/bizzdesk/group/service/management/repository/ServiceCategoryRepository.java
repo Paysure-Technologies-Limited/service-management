60999fbbbe113932b94c33afdf9061fe93d637df
ushaheu <usha.ukange@wasp.net>
2020-05-16 18:11:52 +0100

    Uodate on Repository

--- src/main/java/com/bizzdesk/group/service/management/repository/ServiceCategoryRepository.java
+++ src/main/java/com/bizzdesk/group/service/management/repository/ServiceCategoryRepository.java
@@ -2,6 +2,8 @@ package com.bizzdesk.group.service.management.repository;
 
 import com.bizzdesk.group.service.management.entities.ServiceCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServiceCategoryRepository extends JpaRepository<ServiceCategory, String> {
 }
--- src/main/java/com/bizzdesk/group/service/management/repository/ServiceRepository.java
+++ src/main/java/com/bizzdesk/group/service/management/repository/ServiceRepository.java
@@ -3,9 +3,11 @@ package com.bizzdesk.group.service.management.repository;
 import com.bizzdesk.group.service.management.entities.ServiceCategory;
 import com.bizzdesk.group.service.management.entities.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ServiceRepository extends JpaRepository<Services, Long> {
 
     List<Services> findByServiceCategory(ServiceCategory serviceCategory);
